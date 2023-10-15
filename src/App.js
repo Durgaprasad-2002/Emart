9bff308f72f70a53b425a81104075cbd54728ee8
Durga Prasad <84697619+Durgaprasad-2002@users.noreply.github.com>
2023-10-15 14:32:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" index element={<Index />} />
-          <Route path="/product" element={<ProductInfo />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Routes>
+      <Route path="/" index element={<Index />} />
+      <Route path="/product" element={<ProductInfo />} />
+    </Routes>
   );
 }
 
